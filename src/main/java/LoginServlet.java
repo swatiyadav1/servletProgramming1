ee0af6d3a4f8af6f274be146dc6f9285dc82f2da
swatiyadav1 <108142625+swatiyadav1@users.noreply.github.com>
2022-07-11 20:43:43 +0530

    Update LoginServlet.java

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,6 +1,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,8 +28,36 @@ throws ServletException, IOException
 	PrintWriter p = response.getWriter();
 	String mesg="username is :"+userName+"<br/> Password is :"+pass ;
 	
+	
+	String message="";
+	String values[]=request.getParameterValues("Checkboxgrp");
+	
+	if(values!=null) {
+		for(int i=0;i<values.length;i++) {
+			message=message+"<br/>"+values[i];
+					
+		}
+	}
+	
+	//enumration code for getparameternames
+	
+ Enumeration<String> pEnum =request.getParameterNames();
+ StringBuffer b = new StringBuffer();
+ 
+	while(pEnum.hasMoreElements()) {
+		String eName = pEnum.nextElement();
+		String prName = request.getParameter(eName);
+		b.append(eName + ":" + prName);
+		b.append("<br/>");
+	}
+	
+	
 	p.println(mesg);
 	
+	p.println(message);
+	
+	
+	p.println(b.toString());
 }
 }
  
